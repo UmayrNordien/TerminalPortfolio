97603f6bc0878bbb711c3701e90602fa27e82a59
UmayrNordien <umayrnordien@gmail.com>
2023-09-11 15:51:18 +0200

    Initial commit

---
+++ js/commands.js
@@ -0,0 +1,100 @@
+var youtube = "https://www.youtube.com/playlist?list=PLyQWNkDwdITpAKN9U15y8yvtRte4yikJ8";
+var password = "0827";
+var linkedin = "https://www.linkedin.com/in/umayr-nordien-0b87b1263/";
+var instagram = "https://www.instagram.com/umayrnordien/";
+var github = "https://github.com/UmayrNordien";
+var email = 'umayrnordien@gmail.com';
+
+whois = [
+  "<br>",
+
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "I am me, and the paradox of 'Who am I?' is a question I often ponder.",
+  "It's fascinating how we're never entirely sure of our true selves, yet through life's journey,",
+  "we continually unveil new facets of our identity.",
+  "It's a journey of self-discovery that keeps me intrigued,",
+  "always striving to be the best version of myself.",
+  "<br>"
+];
+
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">My YouTube playlist' + "</a>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/umayrnordien' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/umayrnordien' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/umayrnordien' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  'real ones soon tho :3',
+  '    _,--._.-,',
+  '  /\\_r-,\\_ )',
+  '.-.) _;=\'_/ (.;',
+  '\\ \\\'     \\/S )',
+  ' L.\'-. _.\'|-\'',
+  '<_`-\'\\\'_.\'/',
+  '  `\'-._( \\',
+  '   ___   \\\\',
+  '   \\ .\'-. \\\\   .-\'_. /',
+  '    \'._\'.\'.\\/.-\'_.\'',
+  '       \'--``\\(\'--\'',
+  '             \\\\',
+  '             `\\\\,',
+  '               \\|',
+  '',
+  '                  _         _',
+  '              .-~" "~-. .-~" "~-.',
+  '            __ ... . -^-- ,- -- - - -^ ,._   .',
+  '   _. - ~`v                          ( `,  `,=\')---.-.',
+  ' \'           `.                 .\'   `\'   ;."~-._` )`',
+  ' `.            `._           _.\'           {      )\'~-,',
+  '                  `-.     .-\'               `-  /\'( >~',
+  '                     `. .\'                    `(_.-\'',
+  '                       v',
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          The Enigma of Umayr Nordien',
+  '<span class="command">whoami</span>         Who Am I?',
+  '<span class="command">video</span>          Watch the Chronicles',
+  '<span class="command">social</span>         Reveal the Networks',
+  '<span class="command">secret</span>         Seek the Forbidden Knowledge',
+  '<span class="command">projects</span>       Explore the Creations',
+  '<span class="command">history</span>        Relive the Commands of the Past',
+  '<span class="command">help</span>           The Omniscient Assistance',
+  '<span class="command">email</span>          The Unreachable Realm',
+  '<span class="command">clear</span>          Erase the Record',
+  '<span class="command">banner</span>         Display the Manifesto',
+  "<br>",
+];
+
+
+const banner = [
+  '<span class="color2">Welcome to my interactive Portfolio terminal.</span>',
+  '<pre>',
+  '╦ ╦┌┬┐┌─┐┬ ┬┬─┐  ╔╗╔┌─┐┬─┐┌┬┐┬┌─┐┌┐┌          ',
+  '║ ║│││├─┤└┬┘├┬┘  ║║║│ │├┬┘ │││├┤ │││          ',
+  '╚═╝┴ ┴┴ ┴ ┴ ┴└─  ╝╚╝└─┘┴└──┴┘┴└─┘┘└┘          ',
+  '╦┌┐┌┌┬┐┌─┐┬─┐┌┐┌  ╔╦╗┌─┐┬  ┬┌─┐┬  ┌─┐┌─┐┌─┐┬─┐',
+  '║│││ │ ├┤ ├┬┘│││   ║║├┤ └┐┌┘├┤ │  │ │├─┘├┤ ├┬┘',
+  '╩┘└┘ ┴ └─┘┴└─┘└┘  ═╩╝└─┘ └┘ └─┘┴─┘└─┘┴  └─┘┴└─',
+  '</pre>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
